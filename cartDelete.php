6c849f42f7bbff5acbbf9e098dddff7b99e54860
movie19921209 <movie19921209@qq.com>
2016-09-18 23:56:24 +0800

    update

--- cartDelete.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	header("Content-Type:text/plain;charset=utf-8");
-	$cid=$_REQUEST['cid'];
-	include('connect.php');
-	$sql="DELETE FROM cart WHERE cid='$cid'";
-	$result=mysqli_query($conn,$sql);
-	
- ?>
